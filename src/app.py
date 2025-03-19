8f8ee3210f1b469bef9454bff48393ab19c01019
Jesús Erro <olagato@gmail.com>
2025-03-19 14:10:47 +0100

    chore: commit save point

--- src/app.py
+++ src/app.py
@@ -6,12 +6,11 @@ import seaborn as sns
 # Título de la aplicación
 st.title("¡Bienvenidas a Weather Prediction!")
 
-# ...existing code...
-
 # Cargar el archivo CSV
 def load_data():
     data = pd.read_csv('data/raw/weather_classification_data.csv')
     st.session_state.datos_cargados = True 
+    st.session_state.data = data
     return data
 
 # Función para limpiar los datos
@@ -20,10 +19,6 @@ def clean_data(data):
     data.columns = data.columns.str.lower().str.replace(' ', '_')
     
     # Check for data types
-    st.success("Dataset cleaned successfully!")
-    
-    # Save the cleaned data
-    # data.to_csv('data/processed/weather_classification_data.csv', index=False)
     return data
 
 # Sidebar para cargar y mostrar el encabezado del archivo CSV
@@ -34,6 +29,15 @@ if st.sidebar.button("Cargar y mostrar datos"):
 
 # Botón para limpiar los datos
 if st.sidebar.button("Limpiar Data"):
-    data = load_data()
-    cleaned_data = clean_data(data)
-    st.write("Datos limpiados y guardados en 'data/processed/weather_classification_data.csv'")
+    if 'data' in st.session_state:
+        cleaned_data = clean_data(st.session_state.data)
+        st.session_state.cleaned_data = cleaned_data
+        st.write("Datos limpiados y guardados en 'data/processed/weather_classification_data.csv'")
+        st.write(cleaned_data.head())
+        st.success("Dataset cleaned successfully!")
+    else:
+        st.warning("Primero carga los datos.")
+        
+# Mostrar el DataFrame cargado si existe en session_state
+if 'data' in st.session_state:
+    st.write(st.session_state.data.head())
